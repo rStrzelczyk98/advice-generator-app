ba6d85d997f94b82e3fa0c820592e84b67d685f0
Robert Strzelczyk <robertstrzelczyk98@gmail.com>
2023-02-23 12:56:47 +0100

    commit

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const btn = document.querySelector('.btn');
+
+window.addEventListener('load', getAdvice);
+btn.addEventListener('click', getAdvice);
+
+async function getAdvice() {
+  const adviceNumber = document.querySelector('.advice-number');
+  const adviceText = document.querySelector('.advice');
+  const data = await fetch('https://api.adviceslip.com/advice');
+  const dataJSON = await data.json();
+  const { id, advice } = dataJSON.slip;
+  adviceNumber.textContent = id;
+  adviceText.textContent = advice;
+}
