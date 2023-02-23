bfdf4e4d31c7397e360632ee77ac479b37f1fc52
Robert Strzelczyk <robertstrzelczyk98@gmail.com>
2023-02-23 14:33:31 +0100

    update

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const btn = document.querySelector('.btn');
 
 window.addEventListener('load', getAdvice);
 btn.addEventListener('click', () => {
-  buttonActive(btn);
+  btn.blur();
   getAdvice();
 });
 
@@ -16,10 +16,4 @@ async function getAdvice() {
   const { id, advice } = dataJSON.slip;
   adviceNumber.textContent = id;
   adviceText.textContent = advice;
-}
-
-function buttonActive(btn) {
-  btn.blur();
-  btn.classList.add('active');
-  setTimeout(() => btn.classList.remove('active'), 1000);
 }
